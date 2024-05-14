02679eed99738f6a7532f68aff94960a8ff11382
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2024-05-13 18:13:33 -0700

    Changed my mind- KeyMapper class will have a method to return frequency as a float rather than *be* an EventHandler itself... but didn't get very far doing it.

--- src/main/java/us/malfeasant/synth/App.java
+++ src/main/java/us/malfeasant/synth/App.java
@@ -3,21 +3,26 @@ package us.malfeasant.synth;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+    private final KeyMapper keyMapper = new KeyMapper();
 
     @Override
     public void start(Stage stage) {
-        KeyMapper keyMapper = new KeyMapper();
         Scene scene = new Scene(new Label("Ahoy!"));
-        scene.setOnKeyPressed(keyMapper);
-        scene.setOnKeyReleased(keyMapper);
+        scene.setOnKeyPressed(e -> keyEvent(e));
+        scene.setOnKeyReleased(e -> keyEvent(e));
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void keyEvent(KeyEvent event) {
+        
     }
 
     public static void main(String[] args) {
